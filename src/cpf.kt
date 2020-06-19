37fc5124d4337594c92fd28a2b6d065efa1edd2e
Lanfig <orlandoaf@gmail.com>
2020-06-19 20:05:45 -0300

    Soma de validação do 1o dígito.

--- src/cpf.kt
+++ src/cpf.kt
@@ -2,6 +2,17 @@
 
 fun main(args : Array<String>) {
 
-    println(args[0])
+    val cpf = args[0]
+    var algarismo : Int
+    var soma = 0
 
+    for ( i in 0..8 ) {
+
+        algarismo = Character.getNumericValue(cpf.get(i))
+        println("Posição $i Algarismo $algarismo Multiplicador ${10 - i}")
+
+        soma += algarismo * (10 - i)
+    }
+
+    println(soma)
 }
